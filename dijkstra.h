133ba1bd92aedab2d91752b373d51d069d95e5fb
Xiaojun Dong <akatsuki6725@gmail.com>
2023-08-23 14:19:28 -0700

    fix: output all info at a time

--- dijkstra.h
+++ dijkstra.h
@@ -36,8 +36,10 @@ void verifier(size_t s, const Graph &G, const sequence<EdgeTy> &act_dist) {
   printf("dijkstra running time: %-10f\n", tm.total_time());
   parallel_for(0, G.n, [&](size_t i) {
     if (exp_dist[i] != act_dist[i]) {
-      cout << "exp_dist[" << i << "]: " << exp_dist[i] << ", act_dist[" << i
-           << "]: " << act_dist[i] << '\n';
+      string out =
+          "exp_dist[" + to_string(i) + "]: " + to_string(exp_dist[i]) + " vs. ";
+      out += "act_dist[" + to_string(i) + "]: " + to_string(act_dist[i]) + "\n";
+      cout << out;
     }
     assert(exp_dist[i] == act_dist[i]);
   });
